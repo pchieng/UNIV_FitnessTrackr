81e82a58ce2f12925c9fcca5b348385f9b679398
Peter Chieng <pchieng3@gmail.com>
2022-05-03 12:26:09 -0500

    Links added

---
+++ src/components/NavBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = (props) => {
+
+    return (
+        <div id="navBar">
+            <div id='siteName' className='navText'>
+                Alpha Fitness Tracker
+            </div>
+            <div id="siteLinks">
+
+            <Link to = '/'>
+                <a className='navText'>HOME</a>
+            </Link>
+
+            <Link to = '/routines'>
+                <a className='navText'>ROUTINES</a>
+            </Link>
+
+            <Link to = '/myroutines'>
+                <a className='navText'>MY ROUTINES</a>
+            </Link>
+
+            <Link to = '/activities'>
+                <a className='navText'>ACTIVITIES</a>
+            </Link>
+
+
+            </div>
+
+
+
+        </div>
+
+
+
+    )
+
+}
+
+export default NavBar;
