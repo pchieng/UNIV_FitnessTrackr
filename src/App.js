89b50b166473fea1563fd1d78a1d5b87f812600b
Johnny Hill <jvhill728@gmail.com>
2022-05-02 20:44:24 -0500

    main react skeleton

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { host } from "pg/lib/defaults";
+import  React, { useState } from "react";
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default App;
+
+
